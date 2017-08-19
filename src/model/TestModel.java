2a523fd4cfe4330e9b2a4b030c1f2b00d715e6a1
Emilio <Emilio@DESKTOP-IQFQIRD.fritz.box>
2017-08-19 23:24:55 +0200

    Funzione inserisci e cerca

--- src/model/TestModel.java
+++ src/model/TestModel.java
@@ -5,6 +5,7 @@ public class TestModel {
 	public static void main(String[] args) {
 
 		Model m = new Model();
+		
 		m.addEsame(new Esame("03FY1", "tecniche di pgm1", "fulvio"));
 		m.addEsame(new Esame("03FY2", "tecniche di pgm2", "fulvio"));
 		m.addEsame(new Esame("03FY3", "tecniche di pgm3", "fulvio"));
